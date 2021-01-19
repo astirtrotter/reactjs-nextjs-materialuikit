53073ad43263dd611496de317fceb0f9cf6b6465
astirtrotter <astirtrotter@gmail.com>
2021-01-20 05:58:41 +0800

    Minor update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { AppProps } from 'next/dist/next-server/lib/router/router'
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
@@ -7,7 +6,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../app/styles/theme';
 
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }) => {
     React.useEffect(() => {
         // Remove the server-side injected CSS.
         const jssStyles = document.querySelector('#jss-server-side');
