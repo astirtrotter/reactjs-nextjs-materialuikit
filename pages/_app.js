47806b5baa14ae9778c396e12ba9ba936b595199
astirtrotter <astirtrotter@gmail.com>
2021-01-21 05:41:01 +0800

    Updating basic page layouts

--- pages/_app.js
+++ pages/_app.js
@@ -4,30 +4,31 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../app/styles/theme';
+import "../app/assets/scss/material-kit-react.scss?v=1.9.0";
 
 
 const MyApp = ({ Component, pageProps }) => {
     React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-          jssStyles.parentElement.removeChild(jssStyles);
-        }
-      }, []);
-    
-      return (
-        <React.Fragment>
-          <Head>
-            <title>Personal Website</title>
-            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-          </Head>
-          <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </React.Fragment>
-      );
+      // Remove the server-side injected CSS.
+      const jssStyles = document.querySelector('#jss-server-side');
+      if (jssStyles) {
+        jssStyles.parentElement.removeChild(jssStyles);
+      }
+    }, []);
+  
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Personal Website</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </React.Fragment>
+    );
 }
 
 MyApp.propTypes = {
